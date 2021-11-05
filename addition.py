85481cbd409d465e1676b435ba1d4a74b95afdff
Emanuele Miotto <mail4spam96@gmail.com>
2021-11-05 21:00:21 +0100

    addition and subkey_generation modules
    Changed from arrays to list

---
+++ addition.py
@@ -0,0 +1,21 @@
+"""
+addition()
+@description: sub_key sum function
+@params:'sub_key' previously computed subkey of the original key K
+        @type list of integers
+        @dimension (1x4)
+        'w' output of the previous time stap
+        @type list of integers
+        @dimension (1x8)
+        'p' prime number
+        @type integer
+        @dimension (1x1)
+@output:'v' result of the summations modulo p
+        @type list of integers
+        @dimension (1x8)
+"""
+import numpy as np
+
+def addition (sub_key, w, p):
+    v = list((w + np.array(sub_key+sub_key))%p)
+    return v
---
+++ subkey_generation.py
@@ -0,0 +1,21 @@
+"""
+subkey_generation()
+@description: sub_key generating function
+@params:'K' the original key
+        @type list of integers
+        @dimension (1x8)
+@output:'K_0','K_1','K_2','K_3','K_4','K_5' generated subkeys
+        @type list of integers
+        @dimension (1x4)
+"""
+def subkey_generation (K):
+    K_0 = [K[0],K[2],K[4],K[6]]
+    K_1 = [K[0],K[1],K[2],K[3]]
+    K_2 = [K[0],K[3],K[4],K[7]]
+    K_3 = [K[0],K[3],K[5],K[6]]
+    K_4 = [K[0],K[2],K[5],K[7]]
+    K_5 = [K[2],K[3],K[4],K[5]]
+    return K_0,K_1,K_2,K_3,K_4,K_5
+
+K=[1,0,0,0,0,0,0,0]
+print(subkey_generation(K))
