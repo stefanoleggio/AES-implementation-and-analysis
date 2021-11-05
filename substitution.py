69e6cfeed71b281ec8861a564a91e075a3d642a1
stefano28 <stefanoleggio28@gmail.com>
2021-11-05 23:39:58 +0100

    bug fixing

--- substitution.py
+++ substitution.py
@@ -1,6 +1,14 @@
+
+
+from linear import linear
+
+
 def linear_substitution(v, p):
-    f = lambda x: 2 * x % p
+    f = lambda x: (2 * x) % p
     y = []
     for v_i in v:
         y.append(f(v_i))
     return y
+
+
+print(linear_substitution([1,2,3,4],11))
