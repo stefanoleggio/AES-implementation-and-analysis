ecd73393f14d7c373b32e94f03ad5548de4cc843
stefano28 <stefanoleggio28@gmail.com>
2021-11-08 17:56:08 +0100

    utils functions for inverting matrix and file reading

--- utils.py
+++ utils.py
@@ -1,5 +1,28 @@
+import numpy as np
+
 def modular_inverse(a,m):
     for x in range(1,m):
         if((a%m)*(x%m) % m==1):
             return x
     raise Exception('The modular inverse does not exist.')
+
+def matrix_inverse(A,p):
+    A_inv = np.linalg.inv(A)
+    det_a = round((np.linalg.det(A)))
+    det_a_inv = modular_inverse(det_a,p)
+    return (det_a * A_inv * det_a_inv)%p
+
+
+def read_file(path):
+    file = open(path, "r")
+    u = []
+    x = []
+    for line in file:
+        tmp = line.split("]")
+        u_i = tmp[0].split("[")
+        u_i = np.array(u_i[1].split(",")).astype(int)
+        x_i = tmp[1].split("[")
+        x_i = np.array(x_i[1].split(",")).astype(int)
+        u.append(u_i)
+        x.append(x_i)
+    return u,x
